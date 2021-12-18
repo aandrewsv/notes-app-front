e2e6cb8672066a0d12f18607d0e4e98c6497dc72
aandrewsv <agustin.andrews@gmail.com>
2021-12-18 14:00:02 -0300

    material ui downgrade and more settings

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,33 +1,42 @@
-import React from 'react'
-import { Container, Typography, } from '@mui/material'
-import Button from '@mui/material/Button';
-import { } from '@mui/material';
+import React from 'react';
+import { Container, Typography } from '@material-ui/core';
+import { Button } from '@material-ui/core';
+import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
+import { ThemeProvider, createTheme } from '@material-ui/core';
 
+const theme = createTheme({
+    palette: {
+        background: {
+            secondary: 'violet',
+        },
+    },
+});
 export default function Create() {
-  return (
-    <Container>
-      <Typography
-        variant="h6"
-        color="textSecondary"
-        component="h2"
-        gutterBottom
-      >
-        Create a New Note
-      </Typography>
-      <Button
-        onClick={() => console.log('You Clicked Me')}
-        type='submit'
-        color="secondary"
-        variant='contained'
-      >
-        Submit
-      </Button>
-
-      {/* <ButtonGroup color="secondary" variant='contained'>
-				<Button>One</Button>
-				<Button>Two</Button>
-				<Button>Three</Button>
-			</ButtonGroup> */}
-    </Container>
-  )
+    return (
+        <ThemeProvider theme={theme}>
+            <Container>
+                <Typography
+                    variant='h6'
+                    color='textSecondary'
+                    component='h2'
+                    gutterBottom
+                >
+                    Create a New Note
+                </Typography>
+                <Button
+                    sx={{
+                        color: 'background.secondary',
+                        fontSize: 60,
+                    }}
+                    onClick={() => console.log('You Clicked Me')}
+                    type='submit'
+                    color='secondary'
+                    variant='contained'
+                    endIcon={<KeyboardArrowRightIcon />}
+                >
+                    Submit
+                </Button>
+            </Container>
+        </ThemeProvider>
+    );
 }
