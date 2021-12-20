1d13142e3630ad2b1256aca0798810616e4878d2
aandrewsv <agustin.andrews@gmail.com>
2021-12-20 14:19:37 -0300

    Add redirections, styles, and tag colors

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,39 +1,111 @@
 import React from 'react';
-import { makeStyles, Drawer, Typography } from '@material-ui/core';
+import {
+    makeStyles,
+    Drawer,
+    Typography,
+    List,
+    ListItem,
+    ListItemIcon,
+    ListItemText,
+    AppBar,
+    Toolbar,
+} from '@material-ui/core';
+import { AddCircleOutlineOutlined, SubjectOutlined } from '@material-ui/icons';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 const drawerWidth = 240;
 
-const useStyles = makeStyles({
-    page: {
-        background: '#f9f9f9',
-        width: '100%',
-    },
-    drawer: {
-        width: drawerWidth,
-    },
-    drawerPaper: {
-        width: drawerWidth,
-    },
-    root: {
-        display: 'flex',
-    },
+const useStyles = makeStyles((theme) => {
+    return {
+        page: {
+            background: '#f9f9f9',
+            width: '100%',
+            padding: theme.spacing(3),
+        },
+        drawer: {
+            width: drawerWidth,
+        },
+        drawerPaper: {
+            width: drawerWidth,
+        },
+        root: {
+            display: 'flex',
+        },
+        active: {
+            background: '#f4f4f4',
+        },
+        title: {
+            padding: theme.spacing(2),
+        },
+        appbar: {
+            width: `calc(100% - ${drawerWidth}px)`,
+        },
+        toolbar: theme.mixins.toolbar,
+    };
 });
 
 const Layout = ({ children }) => {
     const classes = useStyles();
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const menuItems = [
+        {
+            text: 'My Notes',
+            icon: <SubjectOutlined color='secondary' />,
+            path: '/',
+        },
+        {
+            text: 'Create Note',
+            icon: <AddCircleOutlineOutlined color='secondary' />,
+            path: '/create',
+        },
+    ];
 
     return (
         <div className={classes.root}>
-            // app bar // side drawer
+            {/* App Bar */}
+            <AppBar className={classes.appbar} elevation={0}>
+                <Toolbar>
+                    <Typography>Welcome to the ninja notes website</Typography>
+                </Toolbar>
+            </AppBar>
+            {/* Side Drawer */}
             <Drawer
                 className={classes.drawer}
                 variant='permanent'
                 anchor='left'
                 classes={{ paper: classes.drawerPaper }}
             >
-                <Typography variant='h5'>Ninja Notes</Typography>
+                <div>
+                    <Typography variant='h5' className={classes.title}>
+                        Ninja Notes
+                    </Typography>
+                </div>
+
+                {/* List / Links  */}
+                <List>
+                    {menuItems.map((item) => (
+                        <ListItem
+                            key={item.text}
+                            button
+                            onClick={() => navigate(item.path)}
+                            className={
+                                location.pathname === item.path
+                                    ? classes.active
+                                    : null
+                            }
+                        >
+                            <ListItemIcon>{item.icon}</ListItemIcon>
+                            <ListItemText primary={item.text} />
+                        </ListItem>
+                    ))}
+                </List>
             </Drawer>
-            <div className={classes.page}>{children}</div>
+            <div className={classes.page}>
+                <div className={classes.toolbar}></div>
+                {children}
+            </div>
         </div>
     );
 };
--- src/components/NoteCard.js
+++ src/components/NoteCard.js
@@ -5,12 +5,35 @@ import {
     CardContent,
     IconButton,
     Typography,
+    makeStyles,
+    Chip,
 } from '@material-ui/core';
 import { DeleteOutlined } from '@material-ui/icons';
+
+const useStyles = makeStyles({
+    tag_bg: {
+        backgroundColor: (note) => {
+            if (note.tag == 'work') return '#ffcdd2';
+            if (note.tag == 'money') return '#c8e6c9';
+            if (note.tag == 'todos') return '#b2ebf2';
+            if (note.tag == 'reminders') return '#ffecb3';
+        },
+    },
+    tag_chip_bg: {
+        backgroundColor: (note) => {
+            if (note.tag == 'work') return '#cb9ca1';
+            if (note.tag == 'money') return '#97b498';
+            if (note.tag == 'todos') return '#81b9bf';
+            if (note.tag == 'reminders') return '#cbba83';
+        },
+    },
+});
+
 const NoteCard = ({ note, handleDelete }) => {
+    const classes = useStyles(note);
     return (
         <div>
-            <Card elevation={2}>
+            <Card elevation={2} className={classes.tag_bg}>
                 <CardHeader
                     action={
                         <IconButton onClick={() => handleDelete(note)}>
@@ -18,7 +41,13 @@ const NoteCard = ({ note, handleDelete }) => {
                         </IconButton>
                     }
                     title={note.title}
-                    subheader={note.category}
+                    subheader={
+                        <Chip
+                            className={classes.tag_chip_bg}
+                            label={note.tag}
+                            variant='outlined'
+                        />
+                    }
                 />
                 <CardContent>
                     <Typography variant='body2' color='textSecondary'>
